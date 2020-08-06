8282440d98e2c64e7ebd66dbb1c6ca1483e31a7c
danielraybarber <63087505+danielraybarber@users.noreply.github.com>
2020-08-06 14:24:11 -0500

    Working Map

--- BoldlyGo/Angular-Client/src/app/app.module.ts
+++ BoldlyGo/Angular-Client/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { UserService } from './components/service/user.service';
+import { MapComponent } from './components/map/map.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MapComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  providers: [UserService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
