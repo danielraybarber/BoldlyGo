93133b7eec02e0c5a27a7da9f1bd20a9d96b45d2
danielraybarber <63087505+danielraybarber@users.noreply.github.com>
2020-08-06 15:09:18 -0500

    Added models

---
+++ BoldlyGo/src/main/java/com/tts/BoldlyGo/Model/User.java
@@ -0,0 +1,94 @@
+package com.tts.BoldlyGo.Model;
+
+//import java.util.Date;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+////import javax.validation.constraints.Email;
+//import javax.validation.constraints.NotEmpty;
+//import javax.validation.constraints.Pattern;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//import com.fasterxml.jackson.annotation.JsonProperty.Access;
+//
+//import org.hibernate.annotations.CreationTimestamp;
+//import org.hibernate.validator.constraints.Length;
+//
+//@Entity
+//@Table(name = "USER")
+public class User {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @Column(name = "user_id")
+//    private Long id;
+//
+//    @NotEmpty(message = "Please provide a username")
+//    @Length(min = 3, message = "Your username must have at least 3 characters")
+//    @Length(max = 15, message = "Your username cannot have more than 15 characters")
+//    @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
+//    private String username;
+//
+//    @Length(min = 5, message = "Your password must have at least 5 characters")
+//    @NotEmpty(message = "Please provide a password")
+//    @JsonProperty(access = Access.WRITE_ONLY)
+//    private String password;
+//
+//    // @Email
+//    // private String email;
+//
+//    private int active;
+//
+//    @CreationTimestamp
+//    private Date createdAt;
+//
+//    public User() {
+//
+//    }
+//
+//    // if email is implement, add arg 'String email'
+//    public User(Long id, String username, String password, int active, Date createdAt) {
+//        this.id = id;
+//        this.setUsername(username);
+//        this.password = password;
+//        this.active = active;
+//		this.createdAt = createdAt;
+//      //this.setEmail(email);
+//    }
+//
+//	public String getUsername() {
+//		return username;
+//	}
+//
+//	public void setUsername(String username) {
+//		this.username = username;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//    }
+//    
+//    public int getActive() {
+//        return active;
+//    }
+//
+//    public void setActive(int active) {
+//        this.active = active;
+//    }
+
+	// public String getEmail() {
+	// 	return email;
+	// }
+
+	// public void setEmail(String email) {
+	// 	this.email = email;
+	// }
+}
