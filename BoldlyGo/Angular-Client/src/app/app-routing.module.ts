b72e0034a745283cd7992976feb78118a3636640
jsdanios <jsdanios@gmail.com>
2020-08-07 08:48:28 -0400

    trying to get the map form to show up

--- BoldlyGo/Angular-Client/src/app/app-routing.module.ts
+++ BoldlyGo/Angular-Client/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MapComponent } from './components/map/map.component';
+import { MapComponent } from './map/map.component';
+import { MapEditorComponent } from './map-editor/map-editor.component';
 
-const routes: Routes = [{ path: 'map', component: MapComponent }];
+const routes: Routes = [
+  { path: 'map', component: MapComponent },
+  { path: 'map-editor', component: MapEditorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BoldlyGo/Angular-Client/src/app/app.module.ts
+++ BoldlyGo/Angular-Client/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { UserService } from './components/service/user.service';
-import { MapComponent } from './components/map/map.component';
+import { UserService } from './user.service';
+import { MapComponent } from './map/map.component';
 import { MapEditorComponent } from './map-editor/map-editor.component';
 
 @NgModule({
   declarations: [AppComponent, MapComponent, MapEditorComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule],
   providers: [UserService],
   bootstrap: [AppComponent],
 })
